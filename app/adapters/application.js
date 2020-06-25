89c395788aa1695981a4aacdce5cb66aea70d29e
Robed Beauvile <robedbeauvil@gmail.com>
2020-06-25 15:32:54 -0400

    updated from 9000 to 5000

--- app/adapters/application.js
+++ app/adapters/application.js
@@ -2,6 +2,6 @@ import JSONAPIAdapter from '@ember-data/adapter/json-api';
 
 export default class ApplicationAdapter extends JSONAPIAdapter {
     //change to current host for api
-    host = "http://localhost:9000";
+    host = "http://localhost:5000";
     namespace = "api";
 }
